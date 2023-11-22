62add9b3b3fc5907bb147850fb158f5e783f86ad
vargajanos <janosvarga706@gmail.com>
2023-11-22 09:46:18 +0100

    táblázat

--- Controllers/table.js
+++ Controllers/table.js
@@ -1,37 +1,53 @@
-function getAllSteps(){
+function Tablazat(){
     let tbody = document.querySelector('tbody');
-    let sumSteps = document.querySelector('#sumSteps');
+    let sumOsszeg = document.querySelector('#sumOsszeg');
 
-    axios.get(`${serverURL}/steps/userID/eq/${loggedUser.ID}`).then(res=>{
+    axios.get(`${serverURL}/items/userID/eq/${loggedUser.ID}`).then(res=>{
         let i = 0;
         let sum = 0;
         res.data.sort((a,b) => a.date.localeCompare(b.date));
         res.data.forEach(item => {
+
             let tr = document.createElement('tr');
             let td1 = document.createElement('td');
             let td2 = document.createElement('td');
             let td3 = document.createElement('td');
+            let td4 = document.createElement('td');
+            let td5 = document.createElement('td');
 
             i++;
-            sum += item.steps;
 
             td1.innerHTML = i + '.';
 
             td2.innerHTML = item.date.split('T')[0];
 
-            td3.innerHTML = item.steps;
-            td3.classList.add('text-end');
+            td3.innerHTML = item.type;
+
+            td4.innerHTML = item.tag;
+
+            if(item.type=="kiadás")
+            {
+                td5.innerHTML = -item.amount;
+                sum += -item.amount;
+            }
+            else{
+                td5.innerHTML = item.amount;
+                sum += item.amount;
+            }    
+            td5.classList.add("text-end");
 
             tr.appendChild(td1);
             tr.appendChild(td2);
             tr.appendChild(td3);
+            tr.appendChild(td4);
+            tr.appendChild(td5);
             tbody.appendChild(tr);
         });
 
-        sumSteps.innerHTML = sum;
+        sumOsszeg.innerHTML = sum;
     })
 }
 
 function renderData(){
-    setTimeout(()=>{getAllSteps();}, 200);
+    setTimeout(()=>{Tablazat();}, 200);
 }
